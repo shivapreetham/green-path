df0723bf1f5b679dc5bdad71ebef2f4b22b6bab7
shivapreetham <2005shivapreetham@gmail.com>
2025-07-05 18:48:32 +0530

    env update

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = "mongodb+srv://2005shadowme:S5quug2t2WfmiabD@shatterbox.qksgz.mongodb.net/green-path"
+const MONGODB_URI = process.env.MONGODB_URI;
 
 console.log('MONGODB_URI', MONGODB_URI)
 if (!MONGODB_URI) {
